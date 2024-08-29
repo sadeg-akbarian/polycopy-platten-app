24aa7cb4763a6777619f5ec5c0426acb958f48df
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-08-29 12:24:05 +0200

    feat: initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<template>
+  <div class="whole-area">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
