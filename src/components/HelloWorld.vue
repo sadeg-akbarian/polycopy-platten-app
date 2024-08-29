352fa5fad89f7eb9dc0443450e110f72676dae92
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-08-29 14:44:29 +0200

    feat: conditional rendering implemented for three input/select-Elements, hover-popup added for 'wartungSelection'-Element

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,16 @@
 <script>
 export default {
+  data() {
+    return {
+      userName: "",
+      selectedWork: "Wähle die Arbeit aus",
+      whichMaintanance: "Welche Wartung",
+      displayWartungPopUp: "none",
+      quantity: "",
+      quantityOfOrders: "",
+      quantityOfVinyls: "",
+    };
+  },
   props: {
     msg: String,
   },
@@ -11,8 +22,8 @@ export default {
     <h1>{{ msg }}</h1>
     <div class="input-area">
       <div class="before-start-area">
-        <input type="text" placeholder="Benutzername" />
-        <select>
+        <input type="text" placeholder="Benutzername" v-model="userName" />
+        <select v-model="selectedWork">
           <option disabled selected>Wähle die Arbeit aus</option>
           <option value="Schallplatten waschen">Schallplatten waschen</option>
           <option value="Eingabe Discogs">Eingabe Discogs</option>
@@ -23,17 +34,45 @@ export default {
           <option value="Lager arbeiten">Lager arbeiten</option>
           <option value="Messe Vorbereitung">Messe Vorbereitung</option>
         </select>
-        <select>
+        <select
+          class="wartungSelection"
+          v-show="selectedWork === 'Wartung Maschinen'"
+          v-model="whichMaintanance"
+        >
           <option disabled selected>Welche Wartung</option>
           <option value="Wartung 1">Wartung 1</option>
           <option value="Wartung 2">Wartung 2</option>
           <option value="Wartung 3">Wartung 3</option>
         </select>
+        <div class="wartung-container">
+          <!-- :style="{ display: displayWartungPopUp }" -->
+          <h2>Wartung 1</h2>
+          <p>jvjdfgbhncv,bjflijdflkglgfkjhlhlgljholgjhjkljgfhn</p>
+          <h2>Wartung 2</h2>
+          <p>dkjvdfjbdfbvgfjbhlgfjhlnjgflnjlgkmnjgljmnblkgjnbkl</p>
+          <h2>Wartung 3</h2>
+          <p>dkvdjchnbvklfjbgklifjbgljcgflbfjgclbjcfklbjcljblcjb</p>
+        </div>
       </div>
       <div class="after-start-area">
-        <input type="text" placeholder="Stückzahl" />
-        <input type="text" placeholder="Anzahl Bestellungen" />
-        <input type="text" placeholder="Anzahl Platten" />
+        <input
+          type="text"
+          placeholder="Stückzahl"
+          v-model="quantity"
+          v-show="selectedWork !== 'Bestellungen bearbeiten'"
+        />
+        <input
+          type="text"
+          placeholder="Anzahl Bestellungen"
+          v-model="quantityOfOrders"
+          v-show="selectedWork === 'Bestellungen bearbeiten'"
+        />
+        <input
+          type="text"
+          placeholder="Anzahl Platten"
+          v-model="quantityOfVinyls"
+          v-show="selectedWork === 'Bestellungen bearbeiten'"
+        />
         <div class="explanationForSendenButton">
           Wenn alles richtig eingegeben ist, <br />
           klicke bitte auf den grünen Kreis <br />
@@ -68,10 +107,39 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  position: relative;
 }
 
 .after-start-area {
   align-items: center;
+}
+
+.wartungSelection:hover + .wartung-container {
+  display: block;
+}
+
+.wartung-container {
+  border: 0.1rem solid black;
+  border-radius: 2rem;
+  padding: 0.5rem;
+  position: absolute;
+  left: -24vw;
+  bottom: -1rem;
+  background-color: rgb(255, 255, 0);
+  display: none;
+}
+
+.wartung-container p {
+  font-size: 1rem;
+  margin-top: 0;
+}
+
+h2 {
+  margin-block: 0;
+}
+
+h2 {
+  font-size: 1.5rem;
 }
 
 button {
